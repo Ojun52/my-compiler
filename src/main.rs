12d6a55bc300f0e17a6cdbf93844243f98deafbd
O-Jun-S <jun.shima256@gmail.com>
2025-08-22 15:06:41 +0900

    Made a compiler that compiles an integer.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::{Arg, command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!().arg(Arg::new("code")).get_matches();
+
+    let arg = matches.get_one::<String>("code");
+
+    if let Some(integer) = arg {
+        println!(".intel_syntax noprefix");
+        println!(".globl main");
+        println!("main:");
+        println!("  mov rax, {}", integer);
+        println!("  ret");
+    } else {
+        println!("The number of argument is wrong.")
+    }
 }
