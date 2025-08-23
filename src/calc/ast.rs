16f9973f0fd4558886f3d5066cfdcfaa57d51a89
Ojun52 <jun.shima256@gmail.com>
2025-08-23 15:22:35 +0900

    Added PlusOp.

--- src/calc/ast.rs
+++ src/calc/ast.rs
@@ -4,6 +4,16 @@ pub enum Expr {
     PlusOp(Box<PlusOp>),
 }
 
+impl Expr {
+    /// 式を評価する。
+    pub fn eval(&self) -> i32 {
+        match self {
+            Expr::ConstInt(e) => e.eval(),
+            Expr::PlusOp(e) => e.eval(),
+        }
+    }
+}
+
 // タプルで定義されている。
 /// 32bit整数定数
 pub struct ConstInt(i32);
@@ -14,8 +24,8 @@ impl CosntInt {
         ConstInt(value);
     }
 
-    /// getter。
-    pub fn get(&self) -> i32 {
+    /// 評価関数。
+    pub fn eval(&self) -> i32 {
         self.0
     }
 }
@@ -24,7 +34,7 @@ impl CosntInt {
 fn constint_test() {
     let expect = 55;
     let const_int = ConstInt::new(expect);
-    assert_eq!(const_int.get(), expect);
+    assert_eq!(const_int.eval(), expect);
 }
 
 /// 加法を表す。
@@ -40,4 +50,24 @@ impl PlusOp {
             right_expr,
         }
     }
+
+    /// 加法の評価。
+    pub fn eval(&self) -> i32 {
+        self.left_expr.eval() + self.right_expr.eval()
+    }
+}
+
+#[test]
+fn plus_op_test() {
+    /// 1+(2+3)
+    let plus_op = PlusOp::new(
+        Expr::ConstInt(1),
+        Expr::PlusOp::new(Box::new(PlusOp::new(
+            Expr::ConstInt::new(2),
+            Expr::ConstInt::new(3),
+        ))),
+    );
+
+    let expect = 1 + (2 + 3);
+    assert_eq!(plus_op.eval(), expect);
 }
