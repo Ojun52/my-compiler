1bd91a89a4f9ef4b7a1b939b3b0eb465b0916235
Ojun52 <jun.shima256@gmail.com>
2025-08-24 10:51:21 +0900

    Added paren_expr_parser.

--- src/lang/parser.rs
+++ src/lang/parser.rs
@@ -1,6 +1,7 @@
 use super::ast;
-use nom::IResult;
 use nom::character::complete;
+use nom::{IResult, sequence};
+use nom::{Parser, character};
 
 pub fn const_int_parser(s: &str) -> IResult<&str, ast::ConstInt> {
     let (rest, integer) = complete::digit1(s)?;
@@ -17,4 +18,15 @@ fn const_int_parser_test() {
 
 pub fn expr_parser(s: &str) -> IResult<&str, ast::Expr> {
     const_int_parser(s).map(|(rest, integer)| (rest, ast::Expr::ConstInt(integer)))
+}
+
+pub fn paren_expr_parser(s: &str) -> IResult<&str, ast::Expr> {
+    sequence::delimited(character::char('('), expr_parser, character::char(')')).parse(s)
+}
+
+#[test]
+fn paren_expr_parser_test() {
+    let (_, actual) = paren_expr_parser("(123)").unwrap();
+    let expect = ast::Expr::ConstInt(ast::ConstInt::new(123));
+    assert_eq!(actual, expect);
 }
